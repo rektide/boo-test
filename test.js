d18b26e3201a99d710789468fd936acf47060308
rektide <rektide+scm@voodoowarez.com>
2020-01-11 17:11:09 -0500

    read main source, remove the custom, legacy difference checker engine.

--- test.js
+++ test.js
@@ -1,45 +1,18 @@
 #!/usr/bin/env node
 import assert from "assert"
 import { fork } from "child_process"
+import { promises as fs } from "fs"
 
 export async function test(){
-	// run main
 	const
+		// run main, buf to collect stdout into
 		main= fork( "./main.js", { silent: true}),
-		buf= []
-	// record it's output
+		buf= [],
+		// read main's text
+		mainSource= fs.readFile( js, "utf8")
+	// record main run's output
 	for await( let b of main.stdout){
 		buf.push( b)
-	}
-	// split output into lines
-	const
-		output= buf.join( ""),
-		lines= output.split( "\n")
-
-	// output
-	//console.log(output)
-
-	// check that we have seen all expected output
-	const expected= [
-		"main importing",
-		"main ran",
-		"target-then console",
-		"target-then then",
-		"target-then setTimeout",
-		"++main saw target-then++",
-		"target-normal console",
-		"++main saw target-normal++",
-	]
-	for( const line of lines){
-		if( line=== ""){
-			continue // skip trailing line end
-		}
-		const i= expected.indexOf( line)
-		assert.notEqual( i, -1, `output line was not expected: ${line}`)
-		expected.splice( i, 1)
-	}
-	for( const line of expected){
-		assert.fail( `expected line was not seen: ${line}`)
 	}
 }
 test()
